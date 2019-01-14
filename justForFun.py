0701d438d0a8776f6146804e65280cf732d5480c
Hongyu Shi <shywzt@iCloud.com>
2019-01-14 15:03:20 +0800

    皮一下

---
+++ justForFun.py
@@ -0,0 +1,9 @@
+#Don't take it seriously
+h = int(input('Please enter your hight (Unit: cm): '))
+
+if h<200 and h>=150:
+    print("You are \"normal\"😝")
+elif h>=200:
+    print("Hello tall guy!")
+else:
+    print("Are you adult?")
