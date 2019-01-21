bb829d85ced60e0539d72248451d7a38736b72f0
Hongyu Shi <shywzt@iCloud.com>
2019-01-22 02:32:03 +0800

    fixed some bugs which might lead to crash

--- series_modified.py
+++ series_modified.py
@@ -1,15 +1,41 @@
-n = int(input("Which term to start with?\n>>>"))
+while True:
+    n = input("Which term to start with?\n>>>")
+    if n.isdigit() and int(n) >= 0 and int(n) <= 1000000:
+        n = int(n)
+        break
 n0 = n
-nMax_end = [10, 100, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 1000000]
+
 n_thTerm = input("Please enter n-th term:\n>>>")
-print("\n", end="")
+while True:
+    try:
+        float(eval(n_thTerm))
+    except:
+        print("Error!\nPlease check your formula, and enter again:")
+        n_thTerm = input(">>>")
+    else:
+        break
+
+nMax_end = [10, 100, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 1000000]
+
+i = 0
+while True:
+    if n0 > nMax_end[i]:
+        del nMax_end[i]
+    else:
+        break
+
 _sum = 0
 
 for nMax in nMax_end:
     while n <= nMax:
-        _sum = _sum + eval(n_thTerm)
+        _sum += eval(n_thTerm)
         n = n + 1
-    print("The sum from ", n0, " to ", n-1, "-th term is:\n", _sum, "\n", sep="")
+    print("\nThe sum from ", n0, " to ", n-1, "-th term is:\n", _sum, "\n", sep="")
+    if nMax < 1000000:
+        _continue = input("Enter to continue.\nType in anything else to stop the program\n>>>")
+        if bool(_continue) == True:
+            print("Finished\n")
+            break
 
 feedback = input("Is your problem solved?\n>>>")
 
