468e3fee1707a8a142847c6a94123307e83556ae
Hongyu Shi <shywzt@iCloud.com>
2019-01-22 20:29:12 +0800

    Week 3 Notes

---
+++ CSC1001/Week 3/Notes.py
@@ -0,0 +1,39 @@
+# Flow Control
+
+# comparison operators (do not change value of var)
+# <, >, <=, ==, >=, !=
+# "Boolean" only output T/F
+n1 = 5
+print(n1 == 6)
+print(n1 <= 10)
+print(n1 + n1 <=10)
+
+print("A" > "a")
+print("A" == "a")
+print("A" < "a")
+print("aa" > "ab")
+print("hello" > "bye")
+print("ABB" > "AAC")
+print(7 == 7.0)
+print(1 == 10*0.1)
+print(1 == 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1)
+print(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1)
+bool(0)
+bool(1 * 4)
+bool("")
+bool(" ")
+bool("hello")
+
+# logical operators
+True and False
+True or False
+False or False
+not False or True
+not (False or True)
+not 2 > 3
+
+n = 3
+n == 3 or 4
+
+n == 4 or 5
+n == 4 or n == 6
