8bdf866341e9093b289985ccd746cc6fc879f613
Hongyu Shi <shywzt@iCloud.com>
2019-03-07 01:53:16 +0800

    8-Puzzle Game v1.3

--- CSC1002/Assignment#1_ 8-Puzzle/8-Puzzle.py
+++ CSC1002/Assignment#1_ 8-Puzzle/8-Puzzle.py
@@ -1,27 +1,22 @@
 import random
 
-g_move = 0
-g_board = []
-
 START_BOARD = [[1,2,3],[4,5,6],[7,8,0]]
 LEFT = 'l'
 RIGHT = 'r'
 UP = 'u'
 DOWN = 'd'
 
-def print_board():                    # function to print the board
-    global g_board
+def print_board(board):                    # function to print the board
     print('')
-    for x in range(len(g_board)):
-        for y in range(len(g_board[x])):
-            if g_board[x][y] == 0:
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if board[x][y] == 0:
                 print('  ', ' ', end='')
             else:
-                print('  ', g_board[x][y], end='')
+                print('  ', board[x][y], end='')
         print('\n')
 
-def random_board():                   # function to generate a new puzzle
-    global g_board
+def random_board(board):                   # function to generate a new puzzle
     list1 = [0,1,2,3,4,5,6,7,8]
     random.shuffle(list1)
     inversion = 0
@@ -33,41 +28,41 @@ def random_board():                   # function to generate a new puzzle
                 inversion += 1
     if inversion % 2 == 0:            # check solvability
         while list1 != []:
-            g_board.append(list1[:3])
+            board.append(list1[:3])
             list1 = list1[3:]
     else:
-        random_board()
+        random_board(board)
 
-def move(direction, zero):            # function to move a number
-    global g_board, g_move
-    if (direction == LEFT and zero[1] == 2)or\
-        (direction == RIGHT and zero[1] == 0)or\
-        (direction == UP and zero[0] == 2)or\
-        (direction == DOWN and zero[0] == 0)or\
-        not (direction in [LEFT, RIGHT, UP, DOWN]):
-        print('Invalid move! Please try again!')
-    else:
-        if direction == LEFT:
-            g_board[zero[0]][zero[1]] = g_board[zero[0]][zero[1]+1]
-            g_board[zero[0]][zero[1]+1] = 0
-        elif direction == RIGHT:
-            g_board[zero[0]][zero[1]] = g_board[zero[0]][zero[1]-1]
-            g_board[zero[0]][zero[1]-1] = 0
-        elif direction == UP:
-            g_board[zero[0]][zero[1]] = g_board[zero[0]+1][zero[1]]
-            g_board[zero[0]+1][zero[1]] = 0
-        elif direction == DOWN:
-            g_board[zero[0]][zero[1]] = g_board[zero[0]-1][zero[1]]
-            g_board[zero[0]-1][zero[1]] = 0
-        print_board()
-        g_move += 1
+def operate(board, valid_direction, zero):   # function to move a number
+    while True:
+        direction = input('Input sliding direction (' + valid_direction + ') > ')
+        if (direction == LEFT and zero[1] == 2)or\
+            (direction == RIGHT and zero[1] == 0)or\
+            (direction == UP and zero[0] == 2)or\
+            (direction == DOWN and zero[0] == 0)or\
+            not (direction in [LEFT, RIGHT, UP, DOWN]):
+            print('Invalid move! Please try again!')
+        else:
+            if direction == LEFT:
+                board[zero[0]][zero[1]] = board[zero[0]][zero[1]+1]
+                board[zero[0]][zero[1]+1] = 0
+            elif direction == RIGHT:
+                board[zero[0]][zero[1]] = board[zero[0]][zero[1]-1]
+                board[zero[0]][zero[1]-1] = 0
+            elif direction == UP:
+                board[zero[0]][zero[1]] = board[zero[0]+1][zero[1]]
+                board[zero[0]+1][zero[1]] = 0
+            elif direction == DOWN:
+                board[zero[0]][zero[1]] = board[zero[0]-1][zero[1]]
+                board[zero[0]-1][zero[1]] = 0
+            print_board(board)
+            break
+    return board
 
-def find_zero():                      # function to find where the zero is
-    global g_board
-    zero = None
-    for x in range(len(g_board)):
-        for y in range(len(g_board[x])):
-            if g_board[x][y] == 0:
+def find_zero(board):                      # function to find where the zero is
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if board[x][y] == 0:
                 zero = (x,y)
     return zero
 
@@ -87,32 +82,21 @@ def judge_direction(zero):            # function to judge valid direction
     valid_direction += direction_list[len(direction_list)-1]
     return valid_direction
 
-def ask_direction(valid_direction):   # function to input direction
-    print('Input sliding direction (' + valid_direction + ') >', end = '')
-    direction = input('')
-    return direction
-
-def judge_end_of_game():              # function to judge whether the puzzle is solved
-    global g_board, g_move
-    if g_board == START_BOARD:
-        print('Congratulations! You solved the puzzle in', g_move, 'moves!')
-        end = True
-    else:
-        end = False
-    return end
-
 def new_game():                       # main game loop
-    global g_move
-    random_board()
-    print_board()
-    while not judge_end_of_game():
-        zero = find_zero()
+    move = 0
+    board = []
+    random_board(board)
+    print_board(board)
+    while True:
+        zero = find_zero(board)
         valid_direction = judge_direction(zero)
-        direction = ask_direction(valid_direction)
-        move(direction, zero)
+        board = operate(board, valid_direction, zero)
+        move += 1
+        if board == START_BOARD:
+            print('Congratulations! You solved the puzzle in', move, 'moves!')
+            break
     y_or_n = input('Do you want to start a new game (Y/N)? ')
     if y_or_n == 'Y' or y_or_n == 'y':
-        g_move = 0
         new_game()
 
 input('Welcome to 8-puzzle game, ………\nPress any key to begin >')
