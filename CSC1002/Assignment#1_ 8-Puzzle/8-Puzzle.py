04d23a8e54056c95bfb634ec92be62c264ba3aa5
Hongyu Shi <shywzt@iCloud.com>
2019-03-10 20:47:49 +0800

    v1.3.5: improved coding style

--- CSC1002/Assignment#1_ 8-Puzzle/8-Puzzle.py
+++ CSC1002/Assignment#1_ 8-Puzzle/8-Puzzle.py
@@ -1,17 +1,18 @@
 import random
 
 START_BOARD = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,0]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 0]
 ]
 
-LEFT = 'l'
-RIGHT = 'r'
-UP = 'u'
-DOWN = 'd'
+LEFT = 'a'
+RIGHT = 'd'
+UP = 'w'
+DOWN = 's'
 
-def print_board(p_board):                      # function to print the board
+
+def print_board(p_board):                  # function to print the board
     print('')
     for x in range(len(p_board)):
         print('\t', end='')
@@ -22,50 +23,54 @@ def print_board(p_board):                      # function to print the board
                 print('  ', p_board[x][y], end='')
         print('\n')
 
-def random_board(p_board):                     # function to generate a new puzzle
+
+def random_board(p_board):                 # function to generate a new puzzle
     list1 = [
-        0,1,2,
-        3,4,5,
-        6,7,8
+        0, 1, 2,
+        3, 4, 5,
+        6, 7, 8
     ]
     random.shuffle(list1)
     inversion = 0
-    for i in range(len(list1)):
+    for i in range(len(list1)):            # calculate the total_inversion
         for j in range(i, len(list1)):
             if (list1[i] > list1[j]
                     and list1[i] != 0
                     and list1[j] != 0):
                 inversion += 1
-    if inversion % 2 == 0:                     # check solvability
-        while list1 != []:                     # if total_inversion is odd:
-            p_board.append(list1[:3])          #     the puzzle is unsolvable
-            list1 = list1[3:]                  # ///algorithm from internet///
+    if inversion % 2 == 0:                 # check solvability:
+        while list1 != []:                 # if total_inversion is odd:
+            p_board.append(list1[:3])      # ____the puzzle is unsolvable
+            list1 = list1[3:]              # ///algorithm from internet///
     else:
         random_board(p_board)
 
-def find_zero(p_board):                        # function to find where the zero is
+
+def find_zero(p_board):                    # function to find where the zero is
     for x in range(len(p_board)):
         for y in range(len(p_board[x])):
             if p_board[x][y] == 0:
-                zero = (x,y)
+                zero = (x, y)
     return zero
 
-def judge_direction(p_zero):                   # function to judge valid direction
+
+def judge_direction(p_zero):               # function to judge valid direction
     valid = 'left, right, up, down'
-    if p_zero[1] == 2:                         # take out invalid item from the list
-        valid = valid.replace('left, ','')
+    if p_zero[1] == 2:                     # take out invalid item
+        valid = valid.replace('left, ', '')
     elif p_zero[1] == 0:
-        valid = valid.replace('right, ','')
+        valid = valid.replace('right, ', '')
     if p_zero[0] == 2:
-        valid = valid.replace('up, ','')
+        valid = valid.replace('up, ', '')
     elif p_zero[0] == 0:
-        valid = valid.replace(', down','')
+        valid = valid.replace(', down', '')
     return valid
 
-def operate(                                   # function to move a number
+
+def operate(                               # function to move a number
         p_board,
-        p_valid_direction,                     # valid direction hint
-        p_zero):                               # the position of zero
+        p_valid_direction,                 # valid direction hint
+        p_zero):                           # the position of zero
     while True:
         direction = input(
             'Input sliding direction (' + p_valid_direction + ') >'
@@ -96,7 +101,8 @@ def operate(                                   # function to move a number
             print_board(p_board)
             break
 
-def new_game():                                # main game loop
+
+def new_game():                            # main game loop
     move = 0
     board = []
     random_board(board)
