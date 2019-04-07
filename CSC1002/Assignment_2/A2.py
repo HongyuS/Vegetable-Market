b875941ce941658e4a11af8e67e0b979259e5969
Hongyu Shi <shywzt@iCloud.com>
2019-04-07 11:56:24 +0800

    added test func

--- CSC1002/Assignment_2/A2.py
+++ CSC1002/Assignment_2/A2.py
@@ -26,29 +26,68 @@ def connectSQLServer():    # function to connect SQL server
         print(e)
         quit()
 
-def select():
+def select(tsql="select * from lgu.course order by dept_name"):
     global table
-    title = title_input.value
-    tsql = "select * from lgu.course where title like %s"
+    data = {}
     with sqlConn.cursor(as_dict=True) as cursor:
-        cursor.execute(tsql, title)
+        cursor.execute(tsql)
         rows = cursor.fetchall()
-        data = {}
-        for row in rows:
-            data['id'] = [row['course_id']]
-            data['title'] = [row['title']]
-            data['dept'] = [row['dept_name']]
-            data['credit'] = [row['credits']]
-            data['instructor'] = [row['instructor']]
-    table.source.data = data
+        data['id'] = [row['course_id'] for row in rows]
+        data['title'] = [row['title'] for row in rows]
+        data['dept'] = [row['dept_name'] for row in rows]
+        data['credit'] = [row['credits'] for row in rows]
+        data['instructor'] = [row['instructor'] for row in rows]
+    print(data)
+    return data
 
-def onClick():
-    pass
+def letterOnClick(idx):
+    tsql = "select * from lgu.course \
+            where title like \'{}%\' \
+            order by dept_name".format(list(string.ascii_uppercase)[idx])
+    table.source.data = select(tsql)
 
-def showPlot(attr, old, new):
+def titleOnClick(idx):
+    if idx == 0:
+        tsql = "select * from lgu.course \
+                where title like \'{}%\' \
+                order by dept_name".format(
+                    title_input.value
+                )
+    elif idx == 1:
+        tsql = "select * from lgu.course \
+                where title like \'%{}%\' \
+                order by dept_name".format(
+                    title_input.value
+                )
+    else:
+        tsql = "select * from lgu.course \
+                where title like \'%{}\' \
+                order by dept_name".format(
+                    title_input.value
+                )
+    table.source.data = select(tsql)
+
+def deptOnClick(idx):
+    if idx == 0:
+        tsql = "select * from lgu.course \
+                where dept_name like \'{}%\'\
+                order by dept_name".format(dept_input.value)
+    elif idx == 1:
+        tsql = "select * from lgu.course \
+                where dept_name like \'%{}%\' \
+                order by dept_name".format(dept_input.value)
+    else:
+        tsql = "select * from lgu.course \
+                where dept_name like \'%{}\' \
+                order by dept_name".format(dept_input.value)
+    table.source.data = select(tsql)
+
+def showPlot(attr, old, new):    # function to update plot when dept changes
     global gpa, data, years, p
     for year in years:
-        tsql = "select gpa, count(gpa) as gpa_num from lgu.student where dept_name = \'{}\' and year = {} group by gpa".format(str(new), year)
+        tsql = "select gpa, count(gpa) as gpa_num from lgu.student \
+                where dept_name = \'{}\' and year = {} \
+                group by gpa".format(new, year)
         with sqlConn.cursor(as_dict=True) as cursor:
             cursor.execute(tsql)
             rows = cursor.fetchall()
@@ -58,7 +97,7 @@ def showPlot(attr, old, new):
                         data[str(year)][i] = d['gpa_num']
     source.data = data
     
-def dept_list():
+def dept_list():    # function to get options of the dept selection on tab_2
     tsql = "select dept_name from lgu.student order by dept_name"
     with sqlConn.cursor(as_dict=True) as cursor:
         cursor.execute(tsql)
@@ -72,7 +111,8 @@ def dept_list():
 
 sqlConn = connectSQLServer()
 
-# Course Info
+# +----------+ Course Info +----------+
+
 columns = [
     wd.TableColumn(field='id', title='Course ID'),
     wd.TableColumn(field='title', title='Title'),
@@ -81,9 +121,9 @@ columns = [
     wd.TableColumn(field='instructor', title='Instructor')
 ]
 table = wd.DataTable(
-    source = ColumnDataSource(),
-    columns = columns,
-    width = 800
+    source=ColumnDataSource(),
+    columns=columns,
+    width=800
 )
 
 paragraph = wd.Paragraph(text='option')
@@ -107,12 +147,13 @@ page_info = layout(
     ]
 )
 
-# Statistics
+# +----------+ Statistics +----------+
+
 selectDept = wd.Select(title='Department:', value='', options=dept_list())
 
 colors = ['#c9d9d3', '#718dbf', '#e84d60']
     
-gpa = ['A+', 'A', 'B+', 'B', 'C+', 'C','D+', 'D','F']
+gpa = ['A+', 'A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F']
 years = ['2015', '2016', '2017']
 data = {}
 data['gpa'] = gpa
@@ -137,7 +178,9 @@ page1 = wd.Panel(child=page_info, title='Course Info')
 page2 = wd.Panel(child=page_sta, title='Statistics')
 tabs = wd.Tabs(tabs=[page1, page2])
 
-refresh.on_click(onClick)
+btnGroupLetters.on_click(letterOnClick)
+# btnGroupTitle.on_click(titleOnClick)
+# btnGroupDept.on_click(deptOnClick)
 selectDept.on_change('value', showPlot)
 
 curdoc().add_root(tabs)
